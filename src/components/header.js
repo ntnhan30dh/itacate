a41311fde8689f8e6f96e35214fb1be7ea7550d7
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2021-02-10 18:20:47 +0100

    header 1st draft

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,22 @@
 import React from "react"
 import logo from "../images/logo.png"
-import OrderNow from "./ordernow"
+// import OrderNow from "./ordernow"
 import { Link } from "gatsby"
 
 const Header = props => {
   let menuActive = props.menuState ? "is-inactive" : ""
   let change = props.menuState ? "change" : ""
+  let hidden = props.menuState ? "hidden" : ""
+  // let wFull = !props.menuState ? "w-full" : ""
   return (
     <nav className="nav">
-      <div className="leftDiv">
-        <Link to="/" className="img_div">
+      <div className={`leftDiv `}>
+        <Link to="/">
+        <div className={`container ${hidden}  img_div`}>
           <img src={logo} alt="logo" />
+        </div>
         </Link>
-        <div className={`burgerMenu  ${change}`} onClick={props.toggleMenu} onKeyDown={props.toggleMenu} role = "button" tabIndex={-1}>
+        <div className={`burgerMenu ml-8 ${change}`} onClick={props.toggleMenu} onKeyDown={props.toggleMenu} role = "button" tabIndex={-1}>
           <div class="bar1"></div>
           <div class="bar2"></div>
           <div class="bar3"></div>
@@ -20,8 +24,8 @@ const Header = props => {
       </div>
       <div className={`rightDiv row ${menuActive}`}>
         <ul>
-          <li></li>
-          <li></li>
+          {/* <li></li> */}
+          {/* <li></li> */}
           <li>
             <Link to="/#story">Our Story</Link>
           </li>
@@ -29,15 +33,16 @@ const Header = props => {
             <Link to="/#menu">Menu</Link>
           </li>
           <li>
+            <Link to="/#menu">Oreder Now</Link>
+          </li>
+          <li>
             <Link to="/#follow">Follow Us</Link>
           </li>
           <li>
             <Link to="/#contact">Contact</Link>
-
-            {/* <AnchorLink to="#story" title="Our Story" /> */}
           </li>
         </ul>
-        <OrderNow />
+        {/* <OrderNow /> */}
       </div>
     </nav>
   )
