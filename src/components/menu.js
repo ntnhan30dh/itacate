edd0ab8bf44d625f8cf490405f7b0189fea77b41
Nhan Nguyen <nhan.nguyen@deliveryhero.com>
2021-02-15 18:55:11 +0100

    menu draft 1

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,200 +1,103 @@
 import React from "react"
-import Plx from "react-plx"
-import { graphql, useStaticQuery } from 'gatsby'
-
-
-import OrderNow from './ordernow'
-// import mainPic from "../images/mains-pic.png"
-// import sidesPic from "../images/sides-pic.png"
-// import dipPic from "../images/dip-pic.png"
-import chickenWing from "../images/chicken-wing.png"
-import chickenLeg from "../images/chicken-leg.png"
-import chickenDish from "../images/chicken-dish.png"
-import fries from "../images/fries.png"
-import chickenBoxIcon from "../images/chicken-box_icon.png"
-import mains from "../images/mainsText.png"
-import sides from "../images/sidesText.png"
-import dips from "../images/dipsText.png"
-import dip from "../images/dip.png"
-
+import { graphql, useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+import Img from "gatsby-image"
 
 const Menu = () => {
-  const parallaxRotate = [
-    {
-      start:'self',
-      duration: 1000,
-      properties: [
-        {
-          startValue: 0,
-          endValue: 180,
-          property: "rotate"
-        },
-      ],
-    },
-  ];
-
-  const parallaxScaleUp = [
-    {
-      start:'self',
-      duration: 500,
-      properties: [
-        {
-        startValue: 0,
-        endValue: 1,
-        property: "scale"
-        },
-      ],
-    },
-  ];
-
-  const parallaxMoveUp = [
-    {
-      start:'self',
-      duration: 500,
-      properties: [
-        {
-          startValue: 100,
-          endValue: 0,
-          property: "translateY"
-        },
-      ],
-    },
-  ];
-
   const data = useStaticQuery(graphql`
-  {
-    mains: file(relativePath: { eq: "mains-pic.png" }) {
-      childImageSharp {
-        fluid(quality: 90, maxWidth: 1000) {
-          ...GatsbyImageSharpFluid_withWebp
+    {
+      bg: file(relativePath: { eq: "burrito-combo-x3.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      iconSet: file(relativePath: { eq: "menu-icon-set.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 700) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      burritoVeggie: file(relativePath: { eq: "burrito-veggie.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 700) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+      brBeefBadget: file(relativePath: { eq: "br-beef-badget.png" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 200) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
         }
       }
     }
-    sides: file(relativePath: { eq: "sides-pic.png" }) {
-      childImageSharp {
-        fluid(quality: 90, maxWidth: 1000) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-    dip: file(relativePath: { eq: "dip-pic.png" }) {
-      childImageSharp {
-        fluid(quality: 90, maxWidth: 1000) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-
-  }
-`)
+  `)
+  const imageData = data.bg.childImageSharp.fluid
   return (
     <section className="menuContainer">
-    <span id="menu"  name="menu"></span>
-      <div className="textTopDiv row ">
-        <div className="chickenWingDiv">
-          <Plx parallaxData={parallaxScaleUp} className="chickenWingImgDiv">
-            <img src={chickenWing} alt="chicken wing" />
-          </Plx>
-        </div>
-        <div className="text">
-          <h1>MENU</h1>
-          <h2>Hungry for wings? We've got just the thing.</h2>
-          <p>
-            Check out our yummy fried chicken served with tongue-tingling
-            sauces.
+      <span id="menu" name="menu"></span>
+      <span className="before"></span>
+      <div className="pic-red-container  w-full flex relative	">
+        <span className="after"></span>
+        <BackgroundImage
+          Tag="section"
+          fluid={imageData}
+          background-size="cover"
+          className="pic-container w-1/2"
+        ></BackgroundImage>
+
+        <div className="red-container container w-1/2 bg-red p-20 ">
+          <Img
+            className={"w-1/2 mx-auto my-10"}
+            fluid={data.iconSet.childImageSharp.fluid}
+          />
+          <p className="text-2xl text-yellow font-semibold w-1/2 mx-auto">
+            Order for your movie night in or an exciting lunch time treat -
+            because, let’s face it, life is always better with burritos.
           </p>
-          <OrderNow/>
-        </div>
-        <div className="chickenLegDiv">
-          <Plx parallaxData={parallaxScaleUp} className="chickenLegImgDiv">
-            <img src={chickenLeg} alt="chicken leg" />
-          </Plx>
         </div>
       </div>
-      <div className="mainDiv row">
-        <div className="mainDivImgContainer">
-          <img src={data.mains.childImageSharp.fluid.src} alt="main menu" />
-          {/* <Img
-              fluid={data.mains.childImageSharp.fluid}
-            /> */}
-        </div>
-        <div   className="mainText">
-          <div className="textContainer">
-          <div className="mainsPicDiv">
-            <img src={mains} alt="mains" />
-          </div>
-          <p>
-            Chicken wings <br />
-            Tenders <br />
-            Popcorn chicken <br />
-            Drumsticks <br />
-          </p>
-          </div>
-          <Plx parallaxData={parallaxRotate} className="chickenDishDiv">
-            <img src={chickenDish} alt="chicken Dish" />
-          </Plx>
-        </div>
-      </div>
-      <div className="mainDiv reverse row">
-        <div className="mainText">
-          <Plx parallaxData={parallaxScaleUp} className="friesDiv">
-            <img src={fries} alt=" fries" />
-          </Plx>
-          <div className="textContainer reverse">
-          <div className="mainsPicDiv">
-            <img src={sides} alt="sides" />
-          </div>
-          <p>
-            Criss cross potatoes <br />
-            Onion rings <br />
-            Cheese nuggets <br />
-            Mozzarella sticks <br />
-            French fries <br />
-            Sweet potato fries <br />
-            Coleslaw <br />
-          </p>
-          </div>
-        </div>
-        <div className="mainDivImgContainer">
-          <img src={data.sides.childImageSharp.fluid.src} alt="sides menu" />
-        </div>
-      </div>
-      <div className="mainDiv row">
-        <div className="mainDivImgContainer">
-          <img src={data.dip.childImageSharp.fluid.src} alt="dips menu" />
-        </div>
-        <div className="mainText">
-          <div className="textContainer">
-          <div className="mainsPicDiv">
-            <img src={dips} alt="dips" />
-          </div>
-          <p>
-            Black pepper <br />
-            Garlic <br />
-            Paprika <br />
-            Bernaise <br />
-            Mango & Jalapeno <br />
-            Cheddar
-            <br />
-          </p>
-          </div>
-          <Plx parallaxData={parallaxScaleUp} className="dipDiv">
-            <img src={dip} alt="dip" />
-          </Plx>
-        </div>
-      </div>
-      <div className="textBottomDiv">
-        <p>
-          Make your fried chicken dreams come true. Pick your favourite dish now and
-          enjoy!
+      <div className="top-text w-full text-center">
+        <p className="text-black  text-xl mt-14">
+          Brace yourself for a Mexican feast of flavours, <br /> from cool
+          guacamole to tangy salsa.
         </p>
-        <OrderNow/>
+        <h1 className="text-green text-2xl font-semibold">
+          EXPLORE OUR MENU TODAY!
+        </h1>
       </div>
-      <div className="bgSmallDiv">
-        <Plx parallaxData={parallaxMoveUp} className="chickenBoxIconDiv">
-          <img src={chickenBoxIcon} alt="chicken box icon" />
-        </Plx>
-\      </div>
+      <div className="menu-item-container flex border-15 border-green w-1/2 mx-auto">
+        <div className="relative w-2/3">
+      <div className="relative">
+        <div className="absolute w-8 z-10 top-1">
+      <Img
+          className={"badget w-full"}
+          fluid={data.brBeefBadget.childImageSharp.fluid}
+        />
+        </div>
+        
+        <Img
+          className={" menu-pic w-full"}
+          fluid={data.burritoVeggie.childImageSharp.fluid}
+        />
+      </div>
+        </div>
+        <div className="menu-list flex flex-col w-1/3">
+          <div className="m-auto w-3/4">
+          <h1 className="text-4xl text-red text-center">BURRITOS</h1>
+          <ul className="text-2xl text-green  text-center">
+            <li>Beef Burrito</li>
+            <li>Chicken Burrito</li>
+            <li>Pork Burrito</li>
+            <li>Veggi Buritto</li>
+          </ul>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    borderWidth: {
+     '15': '15px',
+    },
     extend: {
-      colors:{
-        green: '#0b9446',
-        black: '#2a2c2a'
-      }
+      colors: {
+        green: "#0b9446",
+        black: "#2a2c2a",
+        red: "#d71d24",
+        yellow: "#ffd250",
+      },
     },
   },
   variants: {
